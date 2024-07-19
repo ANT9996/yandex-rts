2484617d844b5fb64653d2b150e3be6f5481c5bc
Alexandr <bocsyr.516@gmail.com>
2024-07-19 21:01:06 +0600

    init

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+    </>
+
+  );
+};
+
+export default Home;
